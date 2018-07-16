d314b15a463a1373be48c9f58c09fbc748e74410
mfurak <mfurak@configit.com>
2018-07-16 14:42:27 +0200

    Update lunch duration

--- time_diff.py
+++ time_diff.py
@@ -12,7 +12,7 @@ while(True):
         first_time = datetime.datetime.strptime("{}:{}".format(first_h, first_m), time_format)
         second_time = datetime.datetime.strptime("{}:{}".format(second_h, second_m), time_format)
         time_diff = second_time - first_time
-        lunch_diff = time_diff - datetime.timedelta(minutes=30)
+        lunch_diff = time_diff - datetime.timedelta(minutes=25)
         print("First time: {} \t Second time: {} \t Diff: {:02d}:{:02d} \t No lunch diff: {:02d}:{:02d}".format(first_time.time().strftime(time_format), second_time.time().strftime(time_format),time_diff.seconds//3600,(time_diff.seconds%3600//60),lunch_diff.seconds//3600,(lunch_diff.seconds%3600//60)))
     except Exception as e:
         print(e)
